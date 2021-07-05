e8987ca3a7c7898bb0b773b5d37a777a527988ef
Liam-13 <83668141+Liam-13@users.noreply.github.com>
2021-07-05 15:00:51 -0400

    Update RunSim.py

--- RunSim.py
+++ RunSim.py
@@ -15,22 +15,30 @@ Input = parser.parse_args()
 
 if __name__ == '__main__':
     StartNow = Initialize() 
+    print('started, 1/8')
     Yaml = yaml.safe_load(open(Input.Yaml, 'r'))
+    print('yaml Loaded 2/8')
     Multi = Multisim.CheckChanges(Yaml)
     if Multi == True: 
         Multisim.CreateYamlCards(Yaml)
     else: 
         import Detector, Simulation
+        print('detector and sim imported 3/8')
         Det = Detector.Detector(Input)
+        print('det function 1 done 4/8')
         Det.ChangeOpticalProperties()
+        print('det function 2 done 5/8')
         Det.Build()
+        print('det function 3 done 6/8')
         Print('Time elapsed in seconds', time.clock())
 
         if(Input.view): 
             Det.View()
 
         Sim = Simulation.Simulation(Det)
+        print('simulating 1 7/8')
         Sim.Simulate()
+        print('simulating 2 8/8')
 
     Print('=')
     Print('Time elapsed in seconds', time.clock())
